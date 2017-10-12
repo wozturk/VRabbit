4cc439c744d1b6faace770863fe263bdbdc35ac1
Wieteke Öztürk-Pots <wieteke.ozturkpots@valtech.nl>
2017-10-12 12:05:38 +0200

    unset bg when camera is on

--- src/scripts/components/start_camera.js
+++ src/scripts/components/start_camera.js
@@ -1,10 +1,12 @@
 AFRAME.registerComponent('start-camera', {
     schema: {
-      on: {type: 'string'}
+      on: {type: 'string'},
+      scene: {type:'selector'}
     },
     init: function () {
       var data = this.data;
       this.el.addEventListener(data.on, () => {
+          data.scene.setAttribute('environment', 'active', false);
           navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia ||  navigator.webkitGetUserMedia || navigator.msGetUserMedia;
           if (!navigator.getUserMedia) {
             return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
